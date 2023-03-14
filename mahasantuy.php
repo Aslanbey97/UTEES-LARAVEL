cf3b4a309fc8745162543daf5c97a6ee3b1d0c2e
Aslanbey97 <107825929+Aslanbey97@users.noreply.github.com>
2023-03-14 21:55:57 +0700

    Add files via upload

---
+++ mahasantuy.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class mahasantuy extends Model
+{
+    use HasFactory;
+    // tambahkan kode untuk mapping ke table mahasantuy
+    protected $table = 'mahasantuy';
+}
